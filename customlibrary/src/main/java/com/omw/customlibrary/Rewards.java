d07b7d029f9185a7e1f1d89b0189be1444e31263
rattangaurav786 <rattan.gaurav@yahoo.com>
2022-06-29 13:58:46 +0530

    new android library

---
+++ customlibrary/src/main/java/com/omw/customlibrary/Rewards.java
@@ -0,0 +1,12 @@
+package com.omw.customlibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class Rewards {
+    public static void showToast(Context c, String message){
+
+        Toast.makeText(c,message, Toast.LENGTH_SHORT).show();
+
+    }
+}
